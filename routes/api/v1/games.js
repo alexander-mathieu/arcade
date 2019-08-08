bcceb4dfc483e514708f42882a11568b0005ad31
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-08-08 17:39:23 -0600

    Add Game update route

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -2,6 +2,23 @@ var express = require('express');
 var router = express.Router();
 var Game = require('../../../models').Game;
 
+router.post('/', function(req, res, next) {
+  Game.create({
+    title: req.body.title,
+    price: req.body.price,
+    releaseYear: req.body.releaseYear,
+    active: req.body.active
+  })
+  .then(game => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(201).send(JSON.stringify(game));
+  })
+  .catch(error => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(500).send({ error });
+  });
+});
+
 router.get('/', function(req, res, next) {
   Game.findAll()
   .then(games => {
@@ -30,16 +47,24 @@ router.get('/:id', function(req, res, next) {
   });
 });
 
-router.post('/', function(req, res, next) {
-  Game.create({
-    title: req.body.title,
-    price: req.body.price,
-    releaseYear: req.body.releaseYear,
-    active: req.body.active
-  })
-  .then(game => {
+router.put('/:id', function(req, res, next) {
+  Game.update(
+    {
+      title: req.body.title,
+      price: req.body.price,
+      releaseYear: req.body.releaseYear,
+      active: req.body.active
+    },
+    {
+      returning: true,
+      where: {
+        id: parseInt(req.params.id)
+      }
+    }
+  )
+  .then(([rowsUpdate, [updatedGame]]) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(201).send(JSON.stringify(game));
+    res.status(202).send(JSON.stringify(updatedGame));
   })
   .catch(error => {
     res.setHeader('Content-Type', 'application/json');
